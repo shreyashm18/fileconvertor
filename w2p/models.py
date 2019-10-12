2f52f1fbf544f13b7fde0bbafe69c898d64ac847
shreyashm18 <musaleshreyash@gmail.com>
2019-10-12 21:24:28 +0530

    Able to upload file from user and file is getting saved in main project folder

--- w2p/models.py
+++ w2p/models.py
@@ -2,6 +2,6 @@ from django.db import models
 
 # Create your models here.
 class Wordfiles(models.Model):
-    wfile=models.FileField(upload_to='files')
+    wfile=models.FileField(upload_to='media')
     fullname=models.CharField(max_length=200)
     img=models.ImageField(upload_to='files')
--- w2p/views.py
+++ w2p/views.py
@@ -1,9 +1,21 @@
-from django.shortcuts import render
-
+from django.shortcuts import render,redirect
+from django.core.files.storage import FileSystemStorage
 # Create your views here.
 
 def home(request):
     return render(request,'home.html')
 
 def w2p(request):
+
+    if request.method=='POST':
+        wfile=request.FILES['wordfile']
+        
+        fs=FileSystemStorage()
+        fs.save(wfile.name,wfile)
+        print(wfile.name)
+        print(wfile.size)
+        print(wfile.content_type)
+        #return redirect('/')
+        
+    
     return render(request,'w2p.html')
